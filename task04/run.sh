b4aa5ad783d7cc43d3c35493b68ebc0a87514db0
asi1024 <akifumiimanishi@yahoo.co.jp>
2014-05-29 16:25:35 +0900

    Add makefile

--- task04/run.sh
+++ task04/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "== bison -d tcc.y"
 bison -d tcc.y
+echo "== flex tcc.l"
 flex tcc.l
+echo "== gcc -o tcc tcc.tab.c lex.yy.c"
 gcc -o tcc tcc.tab.c lex.yy.c
+echo "== ./tcc < test.tc"
 ./tcc < test.tc
